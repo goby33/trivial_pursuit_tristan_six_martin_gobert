78842f243d4db43e6fe1806bad23553ca4ea9e2d
Tristan Six <tarskanfrip@gmail.com>
2022-09-18 15:58:27 +0200

    Classement-page

--- lib/ui/pages/home/classement/classement.dart
+++ lib/ui/pages/home/classement/classement.dart
@@ -3,8 +3,33 @@ import 'package:flutter/material.dart';
 class Classement extends StatelessWidget {
   Classement({Key? key}) : super(key: key);
 
+  var items = List<String>.generate(10000, (i) => 'Item $i');
+
   @override
   Widget build(BuildContext context) {
-    return Text("Classement");
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+              hintText: 'Recherché un pseudo',
+            )
+          ),
+          ListView.builder(
+            itemCount: items.length,
+            prototypeItem: ListTile(
+              title: Text(items.first),
+            ),
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(items[index]),
+                subtitle: Text("Score : "),
+              );
+            },
+          ),
+        ]
+    );
   }
 }
