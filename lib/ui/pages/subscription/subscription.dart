4ddba710c846d1d803cb2bb46708c4080fbdec75
Tristan Six <55380090+Tarskan@users.noreply.github.com>
2022-10-10 16:49:57 +0200

    feat/Subscription page (#5)
    * Subscription-page 1
    
    * Subscription-page 2
    
    * Subscription-page 1
    
    * Subscription-page 2
    
    * feat/subscription-page
    
    Co-authored-by: Martin <martin.gobert@adeo.com>

---
+++ lib/ui/pages/subscription/subscription.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../widgets/bottom_painter.dart';
+import '../../widgets/header_painter.dart';
+
+class Subscription extends StatefulWidget {
+  const Subscription({Key? key}) : super(key: key);
+
+  @override
+  State<Subscription> createState() => _SubscriptionState();
+}
+
+class _SubscriptionState extends State<Subscription> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: CustomPaint(
+              painter: HeaderPainter(),
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width, height: 200),
+            ),
+          ),
+          const Positioned(
+            top: 300,
+            left: 75,
+            child: Text(
+              "Subscription",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(173, 185, 227, 1),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 350,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    TextField(
+                        decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      border: OutlineInputBorder(),
+                      hintText: 'Entrer un pseudo',
+                    )),
+                    TextField(
+                        decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_balance),
+                      border: OutlineInputBorder(),
+                      hintText: 'Entrer un email',
+                    )),
+                    TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.abc),
+                          border: OutlineInputBorder(),
+                          hintText: 'Entrer un mot de passe',
+                        )),
+                  ]),
+            ),
+          ),
+          Positioned(
+            right: 40,
+            top: 40,
+            child: GestureDetector(
+              onTap: () {
+                GoRouter.of(context).push("/");
+              },
+              child: const CircleAvatar(
+                minRadius: 40,
+                backgroundColor: Color.fromRGBO(0, 109, 119, 1),
+                child: Icon(
+                  Icons.arrow_forward,
+                  size: 40,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CustomPaint(
+              painter: BottomPainter(),
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width, height: 200),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/routes/app_routes.dart
+++ lib/ui/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 
 import '../pages/home/home.dart';
 import '../pages/login/login.dart';
+import '../pages/subscription/subscription.dart';
 
 abstract class AppRoutes {
   static const root = '/';
@@ -18,6 +19,12 @@ abstract class AppRoutes {
             name: 'login',
             builder: (context, state) {
               return const Login();
+            }),
+        GoRoute(
+            path: '/subscription',
+            name: 'subscription',
+            builder: (context, state) {
+              return const Subscription();
             }),
       ], initialLocation: '/login', debugLogDiagnostics: true);
 }
