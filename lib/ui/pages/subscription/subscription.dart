afa5ef633a79eac8ae6843b7920c45f72cbd1bf4
Tarskan <55380090+Tarskan@users.noreply.github.com>
2022-10-10 11:34:48 +0200

    Subscription-page 2

--- lib/ui/pages/subscription/subscription.dart
+++ lib/ui/pages/subscription/subscription.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../login/widgets/bottom_painter.dart';
+import '../login/widgets/header_painter.dart';
 
 class subscription extends StatefulWidget {
   const subscription({Key? key}) : super(key: key);
@@ -11,23 +15,80 @@ class subscription extends StatefulWidget {
 class _subscriptionState extends State<subscription> {
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          TextField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
-                hintText: 'Entrer un pseudo',
-              )
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: CustomPaint(
+              painter: HeaderPainter(),
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width, height: 200),
+            ),
           ),
-          TextField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
-                hintText: 'Entrer un mot de passe',
-              )
+          const Positioned(
+            top: 220,
+            left: 200,
+            child: Text(
+              "Subscription",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(173, 185, 227, 1),
+              ),
+            ),
           ),
-      ]
+          Positioned(
+            top: 350,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                          hintText: 'Entrer un pseudo',
+                        )
+                    ),
+                    TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                          hintText: 'Entrer un mot de passe',
+                        )
+                    ),
+                  ]
+              ),
+            ),
+          ),
+          Positioned(
+            right: 40,
+            top: 40,
+            child: GestureDetector(
+              onTap: () {
+                GoRouter.of(context).push("/");
+              },
+              child: const CircleAvatar(
+                minRadius: 40,
+                backgroundColor: Color.fromRGBO(0, 109, 119, 1),
+                child: Icon(
+                  Icons.arrow_forward,
+                  size: 40,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CustomPaint(
+              painter: BottomPainter(),
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width, height: 200),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
