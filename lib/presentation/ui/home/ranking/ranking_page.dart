c620323b38f6ee2bf260b22902a5a588881dcf9f
Tristan Six <tarskanfrip@gmail.com>
2022-12-22 19:01:30 +0100

    feat/ranking_&_fix

--- lib/presentation/ui/home/ranking/ranking_page.dart
+++ lib/presentation/ui/home/ranking/ranking_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:trivial_pursuit_six_tristan_gobert_martin/presentation/states/cubits/ranking_cubit.dart';
+import 'package:trivial_pursuit_six_tristan_gobert_martin/presentation/states/ranking_state.dart';
 import 'package:trivial_pursuit_six_tristan_gobert_martin/presentation/ui/home/ranking/ranking_listener.dart';
 import 'package:trivial_pursuit_six_tristan_gobert_martin/presentation/ui/home/ranking/ranking_provider.dart';
 
@@ -10,40 +13,49 @@ class RankingPage extends StatefulWidget {
 }
 
 class _RankingPageState extends State<RankingPage> {
-  var items = List<String>.generate(10000, (i) => 'Item $i');
 
   @override
   Widget build(BuildContext context) {
     return RankingProvider(
       child: RankingListeners(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          TextField(
-              onChanged: (text) {},
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(),
-                hintText: 'Recherché un pseudo',
-              )),
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              prototypeItem: ListTile(
-                title: Text(items.first),
-              ),
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: const CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823_960_720.jpg',
+        child: BlocBuilder<RankingCubit, RankingState>(
+          builder: (context, state) {
+            if (state is RankingStateLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                      onChanged: (text) {},
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(),
+                        hintText: 'Recherché un pseudo',
+                      )),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: state.listUsersModel!.list_users.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              'https://banner2.cleanpng.com/20180625/req/kisspng-computer-icons-avatar-business-computer-software-user-avatar-5b3097fcae25c3.3909949015299112927133.jpg',
+                            ),
+                          ),
+                          title: Text(state.listUsersModel!.list_users[index].name),
+                          subtitle: Text("Score : ${state.listUsersModel!.list_users[index].score}"),
+                        );
+                      },
                     ),
                   ),
-                  title: Text(items[index]),
-                  subtitle: const Text("Score : "),
-                );
-              },
-            ),
-          ),
-        ]),
+                ],
+              );
+            }
+          },
+        ),
       ),
     );
   }
