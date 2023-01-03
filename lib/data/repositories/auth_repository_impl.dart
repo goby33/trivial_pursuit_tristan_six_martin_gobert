6d5ec00009f3b64ed329ea0a705ef51474d115ec
Goby33 <martin.gobert@hotmail.fr>
2023-01-03 20:55:49 +0100

    fix

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:trivial_pursuit_six_tristan_gobert_martin/config/api_response.dart';
 import 'package:trivial_pursuit_six_tristan_gobert_martin/data/sources/auth_firebase.dart';
 
--- lib/data/sources/user_firebase.dart
+++ lib/data/sources/user_firebase.dart
@@ -2,16 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:trivial_pursuit_six_tristan_gobert_martin/data/models/user/user_model.dart';
 
 class UserFirebase {
-  static final FirebaseFirestore _firebaseFirestore =
-      FirebaseFirestore.instance;
+  static  FirebaseFirestore? _firebaseFirestore;
   static UserFirebase? _instanceUserFirebase;
   static late final CollectionReference<UserModel> _listUsers;
   UserFirebase._();
 
   static UserFirebase getInstance() {
+    _firebaseFirestore ??= FirebaseFirestore.instance;
     if (_instanceUserFirebase == null) {
-      print("UserFirebase instance created");
-      _listUsers = _firebaseFirestore
+      _listUsers = _firebaseFirestore!
           .collection('users')
           .withConverter<UserModel>(
               fromFirestore: (snapshot, _) =>
@@ -22,7 +21,6 @@ class UserFirebase {
     return _instanceUserFirebase!;
   }
 
-  //get user uid
   Future<UserModel?> getUserModel({required String uid}) async {
     final response = await _listUsers.doc(uid).get();
     return response.data();
