3d7e7c4cff6c3e74a85e24b3db8070e56d10a9f4
Tristan Six <tarskanfrip@gmail.com>
2023-01-04 10:29:35 +0100

    feat/dark_theme

--- lib/presentation/style/app_theme.dart
+++ lib/presentation/style/app_theme.dart
@@ -39,11 +39,28 @@ class AppTheme {
   ThemeData darkTheme = ThemeData(
     primaryColor: const Color.fromRGBO(42, 89, 84, 1),
     scaffoldBackgroundColor: const Color.fromRGBO(102, 102, 102, 1),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Color.fromRGBO(42, 89, 84, 1),
+      selectedItemColor: Color.fromRGBO(0, 44, 48, 1),
+      unselectedItemColor: Color.fromRGBO(102, 102, 102, 1),
+    ),
+    bottomAppBarTheme: const BottomAppBarTheme(
+      color: Color.fromRGBO(42, 89, 84, 1),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromRGBO(0, 44, 48, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    ),
     appBarTheme: const AppBarTheme(
       color: Colors.black,
       elevation: 0,
       iconTheme: IconThemeData(color: Colors.white),
     ),
+    inputDecorationTheme: const InputDecorationTheme(),
     textTheme: const TextTheme(
       headline6: TextStyle(
         color: Colors.white,
@@ -51,7 +68,7 @@ class AppTheme {
         fontWeight: FontWeight.bold,
       ),
       headline3: TextStyle(
-        color: Color.fromRGBO(173, 185, 227, 1),
+        color: Color.fromRGBO(49, 121, 146, 1),
         fontSize: 40,
         fontWeight: FontWeight.bold,
       ),
